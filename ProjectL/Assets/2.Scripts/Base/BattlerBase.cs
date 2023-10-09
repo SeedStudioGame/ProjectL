5e57d9ea81a0bc4f464df6af788f970eb1b4d640
2422june <ck66661254@gmail.com>
2023-10-09 14:58:38 +0900

    Fight Prototype ver 0.1

---
+++ ProjectL/Assets/2.Scripts/Base/BattlerBase.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//base of battle objects
+public class BattlerBase : MonoBehaviour
+{
+    private float _speed, firstAttack;
+    public Transform _transform;
+}
