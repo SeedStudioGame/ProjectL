3cafeadad5f068b414930c5cf19adf9d2304fff7
2422june <ck66661254@gmail.com>
2023-12-28 18:00:31 +0900

    이동, 대쉬, 점프, 플랫포머

--- ProjectL/Assets/2.Scripts/Objects/Player/PlayerController.cs
+++ ProjectL/Assets/2.Scripts/Objects/Player/PlayerController.cs
@@ -6,27 +6,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float _h, _v, _moveSpeeed, _jumpPower, _fallPower;
-    private Vector3 _dir, _velovity;
+    private Vector3 _dir, _velovity, _force, _forceImpulse, _preDir;
 
     private Rigidbody _rigid;
 
     private Transform _floorRayPoint;
-    private Ray _floorRay, _wallRay, _flatRay;
+    private Ray _wallRay, _flatRay;
     private RaycastHit _hit;
+
     [SerializeField]
-    private bool _downJumpKey, _isJump, _isOnFloor;
-    [SerializeField]
-    private bool _onAir, _isFall;
-    [SerializeField]
-    private bool _isHeading;
+    private bool _isJump;
+    private bool _downJumpKey, _isOnFloor;
     private float _jumpH, _jumpLimitH, _jumpMinH, _fallLimitV;
 
-    private Transform _flat;
+    private bool _onAir, _isFall;
+
+    [SerializeField]
+    private bool _isHeading;
+
+    [SerializeField]
+    private bool _downDashKey, _isDash;
+    private float _dashTime, _dashTimer, _dashPower;
+    [SerializeField]
+    private float _dashW, _dashLimitW;
+    [SerializeField]
+    private Vector3 _dashDir;
+
+    private Flat _flat;
 
     private void Start()
     {
+        _preDir = Vector3.right;
         _moveSpeeed = 7f;
         SetJumpInfo();
+        SetDashInfo();
 
         SetObjectParameters();
 
@@ -37,10 +50,19 @@ public class PlayerController : MonoBehaviour
     {
         _jumpPower = 15;
         _fallPower = 3;
-        _jumpLimitH = 5;
-        _jumpMinH = 0.3f;
+        _jumpLimitH = 3;
+        _jumpMinH = 0.1f;
         _fallLimitV = -10f;
         _isJump = false;
+    }
+
+    private void SetDashInfo()
+    {
+        _dashTime = 3;
+        _dashTimer = 0;
+        _isDash = false;
+        _dashPower = 60f;
+        _dashLimitW = 5;
     }
 
     private void SetObjectParameters()
@@ -50,33 +72,45 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        _velovity = _rigid.velocity;
+
         KeyInput();
 
         CheckOnFloor();
+        CheckOnFlat();
+        CheckFrontWall();
         CheckBottomFloor();
+
         SetDir();
 
         OnAir();
         Jump();
-        transform.position += _dir.normalized * Time.deltaTime * _moveSpeeed;
+        Dash();
+        Move();
+
+        AddForce();
     }
 
     private void KeyInput()
     {
         _h = Input.GetAxisRaw("Horizontal");
         _downJumpKey = Input.GetKeyDown(KeyCode.Space);
-        //_v = Input.GetAxisRaw("Vertical");
+        _downDashKey = Input.GetMouseButtonDown(1);
     }
 
     private void SetDir()
     {
         _dir.x = _h;
-        _dir.y = _v;
+        
+        if(_dir != Vector3.zero)
+            _preDir = _dir;
     }
 
     private void OnAir()
     {
-        if (_isOnFloor)
+        if (!_isOnFloor)
+            _onAir = true;
+        else
             return;
 
         Fall();
@@ -84,17 +118,12 @@ public class PlayerController : MonoBehaviour
 
     private void Fall()
     {
-        if(!_onAir)
-            return;
-
         _isFall = true;
-        
-        _rigid.AddForce(Vector3.down * _fallPower);
+
+        _force += Vector3.down * _fallPower;
         if(_rigid.velocity.y <= _fallLimitV)
         {
-            _velovity = _rigid.velocity;
             _velovity.y = _fallLimitV;
-            _rigid.velocity = _velovity;
         }
     }
 
@@ -102,67 +131,140 @@ public class PlayerController : MonoBehaviour
     {
         if (_isJump)
         {
-            if (_jumpH + _jumpLimitH <= transform.position.y)
-            {
-                _rigid.velocity = Vector3.up;
+            if (_isHeading)
                 _isJump = false;
-            }
-
-            if(_jumpH + _jumpMinH <= transform.position.y)
-            {
-                if(_isOnFloor)
-                    _isJump = false;
-            }
-            else
-            {
-                if (_isHeading)
-                    _isJump = false;
-            }
+            JumpHeightLimit();
         }
         else
         {
             if(_downJumpKey && _isOnFloor)
             {
                 _jumpH = transform.position.y;
-                _rigid.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+                _forceImpulse += Vector3.up * _jumpPower;
                 _isJump = true;
             }
         }
     }
 
+    private void JumpHeightLimit()
+    {
+        if (_jumpH + _jumpLimitH <= transform.position.y)
+        {
+            _velovity.y = 1;
+            _isJump = false;
+        }
+    }
+
+    private void Move()
+    {
+        transform.position += _dir.normalized * _moveSpeeed * Time.deltaTime;
+    }
+
+    private void Dash()
+    {
+        if(_downDashKey && !_isDash)
+        {
+            _isDash = true;
+            _dashTimer = _dashTime;
+            _dashDir = _preDir;
+            _forceImpulse += _dashDir * _dashPower;
+            _dashW = transform.position.x;
+        }
+
+        if(_isDash)
+        {
+            float dist = _dashW + (_dashDir.x * _dashLimitW) - transform.position.x;
+            Debug.Log(dist);
+            if (dist <= 0.5f && dist >= -0.5f)
+            {
+                _velovity.x = 0;
+            }
+        }
+
+        if (_dashTimer >= 0)
+        {
+            _dashTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _isDash = false;
+            _dashTimer = 0;
+        }
+    }
+
     private void CheckOnFloor()
     {
-        _floorRay.origin = _floorRayPoint.position;
-        _floorRay.direction = Vector3.down;
+        _wallRay.origin = _floorRayPoint.position;
+        _wallRay.direction = Vector3.down;
         _isOnFloor = false;
 
-        if (Physics.Raycast(_floorRay, out _hit, 0.15f, 1 << 6))
+        _wallRay.origin = _floorRayPoint.position + (Vector3.right * -0.3f);
+        if (Physics.Raycast(_wallRay, out _hit, 0.2f, 1 << 6))
         {
             OnLand();
-        }
-        else
-        {
-            _onAir = true;
+            return;
         }
 
-        _flatRay.origin = _floorRayPoint.position;
-        _flatRay.direction = Vector3.down;
-        if (Physics.Raycast(_flatRay, out _hit, 0.15f, 1 << 8))
+        _wallRay.origin = _floorRayPoint.position;
+        if (Physics.Raycast(_wallRay, out _hit, 0.2f, 1 << 6))
         {
-            _flat = _hit.transform;
+            OnLand();
+            return;
+        }
+
+        _wallRay.origin = _floorRayPoint.position + (Vector3.right * 0.3f);
+        if (Physics.Raycast(_wallRay, out _hit, 0.2f, 1 << 6))
+        {
+            OnLand();
+            return;
+        }
+    }
+
+    private void CheckOnFlat()
+    {
+        if (_isOnFloor)
+            return;
+
+        _wallRay.origin = _floorRayPoint.position;
+        _wallRay.direction = Vector3.down;
+
+        Debug.DrawLine(_floorRayPoint.position, _wallRay.origin + (Vector3.down * 0.2f), Color.red);
+
+        _wallRay.origin = _floorRayPoint.position + (Vector3.right * -0.3f);
+        if (Physics.Raycast(_wallRay, out _hit, 0.2f, 1 << 7))
+        {
+            _flat = _hit.transform.GetComponent<Flat>();
             OnFlat();
             OnLand();
+            return;
         }
-        else
+
+        _wallRay.origin = _floorRayPoint.position;
+        if (Physics.Raycast(_wallRay, out _hit, 0.2f, 1 << 7))
         {
-            OffFlat();
-            _onAir = true;
+            _flat = _hit.transform.GetComponent<Flat>();
+            OnFlat();
+            OnLand();
+            return;
         }
+
+        _wallRay.origin = _floorRayPoint.position + (Vector3.right * 0.3f);
+        if (Physics.Raycast(_wallRay, out _hit, 0.2f, 1 << 7))
+        {
+            _flat = _hit.transform.GetComponent<Flat>();
+            OnFlat();
+            OnLand();
+            return;
+        }
+        OffFlat();
     }
 
     private void OnFlat()
     {
-        _flat.GetComponent<Flat>().OnFlat();
+        if (_flat)
+        {
+            _flat.GetComponent<Flat>().OnFlat();
+        }
     }
 
     private void OffFlat()
@@ -177,18 +279,57 @@ public class PlayerController : MonoBehaviour
     {
         _wallRay.origin = _floorRayPoint.position + (Vector3.up * 0.94f);
         _wallRay.direction = Vector3.up;
+
         _isHeading = false;
 
-        if (Physics.Raycast(_wallRay, out _hit, 0.1f, 1 << 7))
+        _wallRay.origin += (Vector3.right * -0.3f);
+        if (Physics.Raycast(_wallRay, out _hit, 0.1f, 1 << 6))
         {
             OnHeading();
+            return;
+        }
+
+        _wallRay.origin += (Vector3.right * 0.3f);
+        if (Physics.Raycast(_wallRay, out _hit, 0.1f, 1 << 6))
+        {
+            OnHeading();
+            return;
+        }
+
+        _wallRay.origin += (Vector3.right * 0.3f);
+        if (Physics.Raycast(_wallRay, out _hit, 0.1f, 1 << 6))
+        {
+            OnHeading();
+            return;
+        }
+    }
+
+    private void CheckFrontWall()
+    {
+        _wallRay.origin = _floorRayPoint.position;
+        _wallRay.direction = Vector3.right * _h;
+
+        if (Physics.Raycast(_wallRay, out _hit, 0.55f, 1 << 6))
+        {
+            _h = 0;
+            _velovity.x = 0;
+            return;
+        }
+
+        _wallRay.origin = _floorRayPoint.position + (Vector3.up * 0.84f);
+
+        if (Physics.Raycast(_wallRay, out _hit, 0.55f, 1 << 6))
+        {
+            _h = 0;
+            _velovity.x = 0;
+            return;
         }
     }
 
     private void OnHeading()
     {
         _isHeading = true;
-        _rigid.velocity = Vector3.zero;
+        //_velovity.y = 0;
     }
 
     private void OnLand()
@@ -196,12 +337,32 @@ public class PlayerController : MonoBehaviour
         if (_rigid.velocity.y > 0)
             return;
 
-        if (_isOnFloor)
-            return;
+        //if (_onAir)
+            _velovity.y = -1;
 
-        _rigid.velocity = Vector3.zero;
         _isOnFloor = true;
         _isFall = false;
         _onAir = false;
+    }
+
+    private void AddForce()
+    {
+        if (_velovity != Vector3.zero || _velovity != _rigid.velocity)
+        {
+            _rigid.velocity = _velovity;
+            _velovity = Vector3.zero;
+        }
+
+        if (_force != Vector3.zero)
+        {
+            _rigid.AddForce(_force);
+            _force = Vector3.zero;
+        }
+
+        if (_forceImpulse != Vector3.zero)
+        {
+            _rigid.AddForce(_forceImpulse, ForceMode.Impulse);
+            _forceImpulse = Vector3.zero;
+        }
     }
 }
